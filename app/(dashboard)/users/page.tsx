f2336851c6ceb553f0461c3461782c37ff08c5a7
panmalo786 <panmalo786@gmail.com>
2026-01-11 08:54:22 -0800

    Delete old home page

--- app/(dashboard)/users/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function UsersPage() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">المستخدمين</h1>
-      <p className="text-muted-foreground">جاري العمل على هذه الوحدة...</p>
-    </div>
-  );
-}
